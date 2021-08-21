d828f0b901b4e84626c0c361e9184b6f872955f5
Brett Barinaga <brettbar@live.com>
2021-08-20 22:01:40 -0700

    assignment 1

--- ts/main.ts
+++ ts/main.ts
@@ -3,7 +3,25 @@ function main() {
 } 
 main();
 
+// Prints "Hello World" to the console
+function helloWorld() {
 
+}
+
+// Returns how many cents the dollars passed in are worth
+function dollarsToCents(dollars: number) {
+
+}
+
+// Returns whether or not a number is even
+function isEven() {
+
+}
+
+// Returns whether or not a number is odd
+function isOdd() {
+
+}
 
 
 
